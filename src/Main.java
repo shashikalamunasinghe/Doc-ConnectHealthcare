3d02b9935567b026307853e66f74accc5fe548b0
shashikalamunasinghe <136602966+shashikalamunasinghe@users.noreply.github.com>
2024-09-01 12:50:53 +0530

    modularized into methods

--- src/Main.java
+++ src/Main.java
@@ -3,61 +3,72 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void hospitalAdministrator() {
+        boolean runAdmin = true;
+        while (runAdmin) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Press 1 to add a doctor,press 2 to add a doctor availability,and press 3 to exit");
+            int userObjective = scanner.nextInt();
+
+            if (userObjective == 1) {
+                System.out.println("added a doctor");
+            } else if (userObjective == 2) {
+                System.out.println("added doctor availability");
+            } else if (userObjective == 3) {
+                runAdmin = false;
+            } else {
+                System.out.println("Invalid input");
+            }
+        }
+    }
+
+    public static void patient(){
+
+        boolean runPatient = true;
+        while (runPatient) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Press 1 to view doctors,press 2 to book an appointment, press 3 to view a selected doctor's bookings,press 4 to register patient and press 5 to exit");
+            int userObjective = scanner.nextInt();
+
+            if (userObjective == 1) {
+                System.out.println("all the doctors");
+            } else if (userObjective == 2) {
+                System.out.println("booked an appointment");
+            } else if (userObjective == 3) {
+                System.out.println("selected doctor's bookings");
+            } else if (userObjective == 4) {
+                System.out.println("register patient");
+            } else if (userObjective == 5) {
+                runPatient = false;
+            } else {
+                System.out.println("Invalid input");
+            }
+        }
+    }
+
+    public static void run() {
 
         boolean runMenu = true;
         while (runMenu) {
-
             System.out.println("If you are a hospital administrator please press 1,If you are a patient please press 2,Press 3 to exit");
             Scanner scanner = new Scanner(System.in);
             int userInput = scanner.nextInt();
 
             if (userInput == 1) {
-
-                boolean runAdmin = true;
-                while (runAdmin) {
-                    System.out.println("Press 1 to add a doctor,press 2 to add a doctor availability,and press 3 to exit");
-                    int userObjective = scanner.nextInt();
-
-                    if (userObjective == 1) {
-                        System.out.println("added a doctor");
-                    } else if (userObjective == 2) {
-                        System.out.println("added doctor availability");
-                    } else if (userObjective == 3) {
-                        runAdmin = false;
-                    } else {
-                        System.out.println("Invalid input");
-                    }
-                }
+                hospitalAdministrator();
             } else if (userInput == 2) {
-
-                boolean runPatient = true;
-                while (runPatient) {
-
-                    System.out.println("Press 1 to view doctors,press 2 to book an appointment, press 3 to view a selected doctor's bookings,press 4 to register patient and press 5 to exit");
-                    int userObjective = scanner.nextInt();
-
-                    if (userObjective == 1) {
-                        System.out.println("all the doctors");
-                    } else if (userObjective == 2) {
-                        System.out.println("booked an appointment");
-                    } else if (userObjective == 3) {
-                        System.out.println("selected doctor's bookings");
-                    } else if (userObjective == 4) {
-                        System.out.println("register patient");
-                    } else if (userObjective == 5) {
-                        runPatient = false;
-                    } else {
-                        System.out.println("Invalid input");
-                    }
-                }
+                patient();
             } else if (userInput == 3) {
-
                 runMenu = false;
                 System.out.println("finished");
-            }else{
+            } else {
                 System.out.println("Invalid input");
             }
         }
+    }
+
+    public static void main(String[] args) {
+
+        run();
     }
 }
